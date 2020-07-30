06d40f2ebdad6750b2f67bb4034dd80c835e62d9
BackToReal1ty <64346571+BackToReal1ty@users.noreply.github.com>
2020-07-31 03:33:16 +0800

    updated help.js

--- Commands/help.js
+++ Commands/help.js
@@ -9,7 +9,11 @@ module.exports = {
                 "\n1. !help - list all available commands" +
                 "\n2. !simps - list  simps" +
                 "\n3. !sorry - say sorry" +
-                "\n4. !expose - expose Elton"
+                "\n4. !expose - expose Elton" +
+                "\n5. !poll - create a random stream" +
+                "\n6. !game - what game is Elton currently playing?" +
+                "\n7. !stream - what game is Elton currently streaming?" +
+                "\n4. !question - let me ask you a random question"
         );
     },
 };
