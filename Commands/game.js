e9dba10f6f594d0eda155f6f504bb9482e2d23e2
BackToReal1ty <64346571+BackToReal1ty@users.noreply.github.com>
2020-07-31 03:01:50 +0800

    Update game.js

--- Commands/game.js
+++ Commands/game.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "game",
     description: "this is a command to list games",
     execute(message, args) {
-        message.channel.send("I am currently playing - \nThe Simps 4: KayXuan Edition");
+        message.channel.send("Elton is currently playing - \nThe Simps 4: KayXuan Edition");
     },
 };
