cf9abd0c6160a86835c966a6ecf4d38dabc8d1b3
BackToReal1ty <64346571+BackToReal1ty@users.noreply.github.com>
2020-08-01 03:38:02 +0800

    updated stream.js

--- Commands/stream.js
+++ Commands/stream.js
@@ -1,9 +1,16 @@
+const {MessageEmbed} = require("discord.js");
+
 module.exports = {
     name: "stream",
     description: "this is a command to list stream",
     execute(message, args) {
-        message.channel.send(
-            "Elton is currently streaming - \n**The Simps 4: KayXuan Edition** \n**Uptime**: Since 27 July 2020"
-        );
+        const Embed = new MessageEmbed()
+            .setColor(12320855)
+            .setTitle("ElmoAngster03")
+            .setDescription("**Watch me Play my Favourite Game!**")
+            .addField("Game:     \tViewers:", "The Simps 4\t69")
+            .addField("Uptime", "Since Since 27 July 2020")
+            .setImage("https://i.imgur.com/3upo5vX.jpeg");
+        message.channel.send("Elton is Currently Streaming on Twitch!\n" + Embed);
     },
 };
