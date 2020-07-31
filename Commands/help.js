5b6b51318a86e71191a8855174109f923b4ed527
BackToReal1ty <64346571+BackToReal1ty@users.noreply.github.com>
2020-08-01 04:05:50 +0800

    updates

--- Commands/help.js
+++ Commands/help.js
@@ -11,9 +11,10 @@ module.exports = {
                 "\n3. !sorry - say sorry" +
                 "\n4. !expose - expose Elton" +
                 "\n5. !poll - create a random stream" +
-                "\n6. !game - what game is Elton currently playing?" +
+                "\n6. !game - check out our channl's sponsor!?" +
                 "\n7. !stream - what game is Elton currently streaming?" +
-                "\n4. !question - let me ask you a random question"
+                "\n8. !question - let me ask you a random question" +
+                "\n9. !photos - see Elton and KayXuan's unreleased photos!"
         );
     },
 };
--- Commands/stream.js
+++ Commands/stream.js
@@ -8,7 +8,9 @@ module.exports = {
             .setColor(12320855)
             .setAuthor("ElmoAngster03", "https://i.imgur.com/bJ9NutH.jpg", "https://www.instagram.com/elmo_ang/")
             .setTitle("ElmoAngster03")
+            .setURL("https://www.instagram.com/elmo_ang/")
             .setDescription("**Watch me Play my Favourite Game!**")
+            .setThumbnail("https://i.imgur.com/sjIf7Es.jpeg")
             .addFields({name: "Game: ", value: "The Simps 4", inline: true}, {name: "Viewers: ", value: "69", inline: true})
             .addField("Uptime:", "Since Since 27 July 2020")
             .setImage("https://i.imgur.com/3upo5vX.jpeg");
