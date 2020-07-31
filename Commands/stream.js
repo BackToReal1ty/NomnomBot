c936a29edcef56c789705d2f29567b397cf714e3
BackToReal1ty <64346571+BackToReal1ty@users.noreply.github.com>
2020-08-01 03:48:14 +0800

    updatd stream.js

--- Commands/stream.js
+++ Commands/stream.js
@@ -8,7 +8,7 @@ module.exports = {
             .setColor(12320855)
             .setTitle("ElmoAngster03")
             .setDescription("**Watch me Play my Favourite Game!**")
-            .addField("Game:          \tViewers:", "The Simps 4\t   69")
+            .addFields({name: "Game: ", value: "The Simps 4", inline: true}, {name: "Viewers: ", value: "69", inline: true})
             .addField("Uptime:", "Since Since 27 July 2020")
             .setImage("https://i.imgur.com/3upo5vX.jpeg");
         message.channel.send("Elton is Currently Streaming on Twitch!\n");
